def80494d235eb1a518dc37d10906a326d7a7b24
Markus <ghsthack@gmail.com>
2020-01-31 23:47:10 +0100

    Simple sfall write_byte generation

---
+++ Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sfall_asm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine(System.AppDomain.CurrentDomain.FriendlyName + " [asm_patch]");
+                return;
+            }
+
+            if (!File.Exists(args[0]))
+            {
+                Console.WriteLine(args[0] + " doesn't exist.");
+                return;
+            }
+
+            var lines = new List<string>();
+            try
+            {
+                lines.AddRange(File.ReadAllLines(args[0]));
+            }
+            catch (Exception ex) { 
+                Console.WriteLine("Unable to open file: " + ex.Message);
+                Environment.Exit(1);
+            };
+
+            var output = new List<string>();
+            var lastOffset = 0;
+            foreach(var line in lines)
+            {
+                if (!line.Contains('|'))
+                    continue;
+
+                var spl = line.Split('|');
+                if (spl.Length != 3)
+                    continue;
+
+                 
+                var offset = Convert.ToInt32(spl[0].Trim(), 16);
+                if (offset == 0)
+                    offset = lastOffset;
+                
+                var bytes = spl[1].Replace(" ", "");
+                for (var i= 0; i < bytes.Length; i+=2)
+                {
+                    var writeByte = $"write_byte(0x{offset.ToString("x")}, 0x{bytes[i]}{bytes[i + 1]})";
+                    if (i == 0)
+                        writeByte += "; /* " + spl[2].Trim() + " */ ";
+                    else
+                        writeByte += ";";
+                    writeByte += " \\";
+                    output.Add(writeByte);
+                    offset++;
+                    lastOffset = offset;
+                }
+            }
+
+            foreach (var l in output)
+                Console.WriteLine(l);
+
+            // Console.ReadKey();
+            //var lines = ;
+            //foreach(var line in lines)
+        }
+    }
+}
---
+++ patches/fill_w.asm
@@ -0,0 +1,37 @@
+// Fill_W that works like in Fallout 1
+// https://github.com/rotators/Fo1in2/issues/16
+
+004C3735 | E9 F576F5FF  | jmp fallout2.41AE2F
+0041AE2F | 75 4D        | jne fallout2.41AE7E
+00000000 | 83EC 04      | sub esp,4 
+00000000 | C60424 00    | mov ss:[esp+4],0 
+00000000 | 8B4C24 0C    | mov ecx,ss:[esp+C]
+00000000 | 49           | dec ecx 
+00000000 | 83F9 03      | cmp ecx,3 
+00000000 | 74 39        | je 0x41AE7E
+00000000 | 83F9 07      | cmp ecx,7
+00000000 | 74 34        | je 0x41AE7E
+00000000 | 83F9 0B      | cmp ecx,B                     
+00000000 | 74 2F        | je 0x41AE7E                   
+00000000 | 83F9 0F      | cmp ecx,F                     
+00000000 | 74 2A        | je 0x41AE7E                   
+00000000 | 31ED         | xor ebp,ebp                   
+00000000 | 894C24 0C    | mov ss:[esp+C],ecx            
+00000000 | 6A 02        | push 2                        
+00000000 | 8B4424 10    | mov eax,ss:[esp+10]           
+00000000 | 89F1         | mov ecx,esi                   
+00000000 | 89FB         | mov ebx,edi                   
+00000000 | 56           | push esi                      
+00000000 | 89EA         | mov edx,ebp                   
+00000000 | 45           | inc ebp                       
+00000000 | E8 CA850A00  | call 0x4c3434
+00000000 | 83FD 07      | cmp ebp,7                     
+00000000 | 7C E8        | jl 41AE58                     
+00000000 | 8B0424       | mov eax,ss:[esp+4]            
+00000000 | 40           | inc eax                       
+00000000 | 83F8 02      | cmp eax,2                     
+00000000 | 890424       | mov ss:[esp+4],eax            
+00000000 | 7C BD        | jl 41AE39           
+00000000 | 83C4 04      | add esp,4           
+00000000 | 83C4 0C      | add esp,C           
+00000000 | E9 B4880A00  | jmp fallout2.4C373A

026767b1dff619a9c9876372229d43c4b4e2279e
Markus <ghsthack@gmail.com>
2020-02-21 01:44:36 +0100

    Patch fixes
    - Made fill_w.asm address independant and documented code.
    - no_location_radius.asm is now address independant too.
    
    We are no longer reliant on rfall for VirtualProtect or _defam stuff - https://github.com/rotators/Fo1in2/issues/41

--- patches/no_location_radius.asm
+++ patches/no_location_radius.asm
@@ -1,26 +1,24 @@
-//! NAME no_location_radius
+//! NAME location_discover_radius
 /// No radius when a location is revealed on the worldmap
 /// Modifies sfall code
 
 //! ASM !//
-/// _defam
-0041AE05 | 60               | pushad
-0041AE06 | 2EA1 70464C00    | mov eax,cs:[4C4670]
+[patch]  | 60               | pushad
+00000000 | 2EA1 70464C00    | mov eax,cs:[4C4670]
 /// Calculate where ddraw.sfall::wmAreaMarkVisitedState_hack+0x51 is
-0041AE0C | BA C5464C00      | mov edx,fallout2.4C46C5
-0041AE11 | 01D0             | add eax,edx
-0041AE13 | 89C6             | mov esi,eax
+00000000 | BA C5464C00      | mov edx,fallout2.4C46C5
+00000000 | 01D0             | add eax,edx
+00000000 | 89C6             | mov esi,eax
 /// esi is now the address where the radius byte is
-/// since the memory of the code address is memory protected, 
+/// since the memory of the code address is read-only protected,
 /// we need to use VirtualProtect to change this
-0041AE15 | 83EC 04          | sub esp,4
-0041AE18 | 54               | push esp
-0041AE19 | 6A 40            | push 40
-0041AE1B | 6A 01            | push 1
-0041AE1D | 50               | push eax
-/// Requires r_fall
-0041AE1E | 2EFF15 04004100  | call cs:[<&VirtualProtect>]
-0041AE25 | C606 00          | mov ds:[esi],0
-0041AE28 | 83C4 04          | add esp,4
-0041AE2B | 61               | popad
-0041AE2C | C3               | ret
+00000000 | 83EC 04          | sub esp,4
+00000000 | 54               | push esp
+00000000 | 6A 40            | push 40
+00000000 | 6A 01            | push 1
+00000000 | 50               | push eax
+00000000 | 2EFF15 18026C00  | call cs:[<&fallout2.VirtualProtect>]
+00000000 | C606 00          | mov ds:[esi],0
+00000000 | 83C4 04          | add esp,4
+00000000 | 61               | popad
+00000000 | C3               | ret

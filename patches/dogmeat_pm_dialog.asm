885f3ecb297eaf0386ab311601001030f2c24be4
Wipe <wipe.git@gmail.com>
2020-02-23 11:07:32 +0100

    typo

--- patches/dogmeat_pm_dialog.asm
+++ patches/dogmeat_pm_dialog.asm
@@ -1,4 +1,4 @@
-//! NAME domeat_pm_dialog
+//! NAME dogmeat_pm_dialog
 /// This will replace RoboDog PID with Dogmeat PID in hardcoded list of dogs PIDs; required for woofs and arfs in combat control
 //  NOTE: FO1@2-specific, as it's writing critter PID
 
